139dbaf43c36be05652992b913a982b85264ad06
Osamu Ishizawa <kiaydr@gmail.com>
2025-08-11 13:10:21 +0900

    feat: clientサーバーの初期化

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Input from "./search/component/Input";
+import Table from "./search/component/Table";
+
+function App() {
+  return (
+    <>
+      <h1>Full Text Search</h1>
+      <Input />
+      <Table />
+    </>
+  );
+}
+
+export default App;
